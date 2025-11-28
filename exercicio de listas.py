945c3a8e359022218df773ec4c94386435cc0144
Raimundo Alves de Oliveira <rayalves9322@gmail.com>
2025-11-28 18:20:02 -0300

    Add files via upload

---
+++ exercicio de listas.py
@@ -0,0 +1,9 @@
+prova_1= ["Matematica", "Ingles", "Filosofia"]
+prova_2= ["Fisica", "Quimica"]
+prova_total = prova_1 + prova_2
+print("lista de prova 1")
+print(prova_1)
+print("lista de prova 2")
+print(prova_2)
+print("provas 1 e 2 juntas")
+print(prova_total)
